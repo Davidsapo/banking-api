40e08c1b598d376fbf07f062d86478416a0630bd
david-sapozhnik-axon <dsapozhnik@axon.dev>
2024-06-12 22:41:40 +0300

    feat: Add TransactionType enum

---
+++ src/main/java/com/fintech/bankingapi/enums/TransactionType.java
@@ -0,0 +1,12 @@
+package com.fintech.bankingapi.enums;
+
+/**
+ * Enum representing the type of transaction that can be performed on an account.
+ *
+ * @author David Sapozhnik
+ */
+public enum TransactionType {
+    DEPOSIT,
+    WITHDRAW,
+    TRANSFER
+}
