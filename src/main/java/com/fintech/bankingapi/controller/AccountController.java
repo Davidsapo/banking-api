bd45ca71865a344ab776eb6f71f83e56618e1bb6
david-sapozhnik-axon <dsapozhnik@axon.dev>
2024-06-13 14:02:43 +0300

    feat: Add account controller

---
+++ src/main/java/com/fintech/bankingapi/controller/AccountController.java
@@ -0,0 +1,50 @@
+package com.fintech.bankingapi.controller;
+
+import com.fintech.bankingapi.model.PaginatedResponse;
+import com.fintech.bankingapi.model.dto.AccountDTO;
+import com.fintech.bankingapi.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/v1/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDTO> createAccount(@RequestParam BigDecimal initialBalance) {
+        AccountDTO account = accountService.createAccount(initialBalance);
+        return ResponseEntity.status(HttpStatus.CREATED).body(account);
+    }
+
+    @GetMapping("/{accountNumber}")
+    public ResponseEntity<AccountDTO> getAccountByAccountNumber(@PathVariable String accountNumber) {
+        AccountDTO account = accountService.getAccountByAccountNumber(accountNumber);
+        return ResponseEntity.ok(account);
+    }
+
+    @GetMapping
+    public ResponseEntity<PaginatedResponse<AccountDTO>> getAllAccounts(Pageable pageable) {
+        Page<AccountDTO> accounts = accountService.getAllAccounts(pageable);
+        PaginatedResponse<AccountDTO> response = new PaginatedResponse<>(accounts);
+        return ResponseEntity.ok(response);
+    }
+}
+
